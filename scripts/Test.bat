13de35a0361c7c69355e969024d357fa5302c2ee
Jennifer <jennny.gc@gmail.com>
2015-09-27 20:23:41 +0200

    Preparacion

---
+++ scripts/Test.bat
@@ -0,0 +1,19 @@
+@echo off
+set workspace=C:\Users\Jennifer\Desktop\ProyectosMaster\Jennifer.Garcia.ecp1.miw.upm.es
+set PATH=%PATH%C:\Program Files\Java\jdk1.8.0_45\bin;C:\Users\Jennifer\Desktop\apache-maven-3.3.3\bin
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_45
+set M2_HOME=C:\Users\Jennifer\Desktop\apache-maven-3.3.3
+echo -----------------------------------------
+echo . (C) MIW
+echo -----------------------------------------
+echo .
+echo Workspace --- %workspace%
+echo .
+cd %workspace%
+echo ============ mvn --version =======================================================
+echo .
+call mvn --version
+call mvn clean test -Denvironment.type=develop
+pause
+::Se queda la consola abierta para realizar comandos en línea
+::cmd /k
